0d303cdedef0656c58f291176798e703629a43e1
jacoobes <jacobnguyend@gmail.com>
2021-09-04 18:31:44 -0500

    first working example of tesseract OCR

--- build/index.js
+++ build/index.js
@@ -1,6 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function readImages(dir) {
-    var filePath = dir.endsWith(".png") ? dir : Error("Not a correct path");
+const promises_1 = require("fs/promises");
+const path_1 = require("path");
+const tesseract_js_1 = require("tesseract.js");
+async function getFiles(dir) {
+    const dirents = await (0, promises_1.readdir)(dir, { withFileTypes: true });
+    const files = await Promise.all(dirents.map((dirent) => {
+        const res = (0, path_1.resolve)(dir, dirent.name);
+        return dirent.isDirectory() ? getFiles(res) : res;
+    }));
+    return files.flat();
+}
+(async () => {
+    console.log(await getFiles("E:\\Downloads\\result"));
+    await readImages("E:\\Downloads\\result\\Chapter 2 - Psychological Research-3.png");
+})();
+async function readImages(dir) {
+    //  const arrayOfFiles = (await getFiles(dir) as string[])
+    const worker = (0, tesseract_js_1.createWorker)();
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize('eng');
+    await worker.setParameters({
+        tessedit_pageseg_mode: "3" /* AUTO */,
+    });
+    const { data: { text } } = await worker.recognize(dir);
+    await worker.terminate();
+    console.log(text);
 }
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,52 @@
-import { readdir, readdirSync } from "fs";
+import { lstatSync } from "fs";
+import { readdir } from "fs/promises";
 import Tesseract from "tesseract.js";
+import  { resolve }  from 'path'
+import { createWorker } from "tesseract.js";
+
+import { PSM } from "tesseract.js"
 
 type successPath = string | Error
 
-function readImages(dir: string) {
-    const filePath : successPath  = dir.endsWith(".png") ? dir : Error("Not a correct path")
+async function getFiles(dir: string) : Promise<string[] | string> {
+    const dirents = await readdir(dir, { withFileTypes: true });
+    const files = await Promise.all(dirents.map((dirent) => {
+      const res = resolve(dir, dirent.name);
+      return dirent.isDirectory() ? getFiles(res) : res;
+    }));
+    return files.flat()
+  }
+
+  (async () => {
+   console.log( await getFiles("E:\\Downloads\\result"))
+
+   await readImages("E:\\Downloads\\result\\Chapter 2 - Psychological Research-3.png")
+  
+  })()
+
+async function readImages(dir: string) {
+    
+
+  //  const arrayOfFiles = (await getFiles(dir) as string[])
+    const worker = createWorker()
+      await worker.load()
+      await worker.loadLanguage("eng")
+      await worker.initialize('eng')
+      await worker.setParameters({
+        tessedit_pageseg_mode: PSM.AUTO,
+      })
+      const { data: {text} } = await worker.recognize(dir)
+
+      await worker.terminate()
+   
+  
+    console.log(text)
+   
+      
+    
+
+
 }
+
+
+
